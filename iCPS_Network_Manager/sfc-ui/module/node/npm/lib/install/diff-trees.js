3a83d7471fb74520fba2716cf70cb66625c7c1c6
jaewook2 <iioiioiio12345@gmail.com>
2018-05-26 20:42:04 +0900

    Network Manager upload

---
+++ iCPS_Network_Manager/sfc-ui/module/node/npm/lib/install/diff-trees.js
@@ -0,0 +1,139 @@
+'use strict'
+var validate = require('aproba')
+var npa = require('npm-package-arg')
+var flattenTree = require('./flatten-tree.js')
+
+function nonRegistrySource (pkg) {
+  validate('O', arguments)
+  var requested = pkg._requested || (pkg._from && npa(pkg._from))
+  if (!requested) return false
+
+  if (requested.type === 'hosted') return true
+  if (requested.type === 'local') return true
+  return false
+}
+
+function pkgAreEquiv (aa, bb) {
+  var aaSha = (aa.dist && aa.dist.shasum) || aa._shasum
+  var bbSha = (bb.dist && bb.dist.shasum) || bb._shasum
+  if (aaSha === bbSha) return true
+  if (aaSha || bbSha) return false
+  if (nonRegistrySource(aa) || nonRegistrySource(bb)) return false
+  if (aa.version === bb.version) return true
+  return false
+}
+
+function getNameAndVersion (pkg) {
+  var versionspec = pkg._shasum
+
+  if (!versionspec && nonRegistrySource(pkg)) {
+    if (pkg._requested) {
+      versionspec = pkg._requested.spec
+    } else if (pkg._from) {
+      versionspec = npa(pkg._from).spec
+    }
+  }
+  if (!versionspec) {
+    versionspec = pkg.version
+  }
+  return pkg.name + '@' + versionspec
+}
+
+function pushAll (aa, bb) {
+  Array.prototype.push.apply(aa, bb)
+}
+
+module.exports = function (oldTree, newTree, differences, log, next) {
+  validate('OOAOF', arguments)
+  pushAll(differences, sortActions(diffTrees(oldTree, newTree)))
+  log.finish()
+  next()
+}
+
+function isLink (node) {
+  return node && node.isLink
+}
+
+function requiredByAllLinked (node) {
+  if (!node.requiredBy.length) return false
+  return node.requiredBy.filter(isLink).length === node.requiredBy.length
+}
+
+var sortActions = module.exports.sortActions = function (differences) {
+  var actions = {}
+  differences.forEach(function (action) {
+    var child = action[1]
+    actions[child.package._location] = action
+  })
+
+  var sorted = []
+  var added = {}
+  Object.keys(actions).sort(sortByLocation).forEach(function (location) {
+    sortByDeps(actions[location])
+  })
+
+  function sortByLocation (aa, bb) {
+    return aa.length - bb.length || bb.localeCompare(aa)
+  }
+  function sortByDeps (action) {
+    var cmd = action[1]
+    if (added[cmd.package._location]) return
+    added[cmd.package._location] = action
+    cmd.package._requiredBy.forEach(function (location) {
+      if (actions[location]) sortByDeps(actions[location])
+    })
+    sorted.unshift(action)
+  }
+  return sorted
+}
+
+function diffTrees (oldTree, newTree) {
+  validate('OO', arguments)
+  var differences = []
+  var flatOldTree = flattenTree(oldTree)
+  var flatNewTree = flattenTree(newTree)
+  var toRemove = {}
+  var toRemoveByNameAndVer = {}
+  // find differences
+  Object.keys(flatOldTree).forEach(function (flatname) {
+    if (flatNewTree[flatname]) return
+    var pkg = flatOldTree[flatname]
+    toRemove[flatname] = pkg
+    var namever = getNameAndVersion(pkg.package)
+    if (!toRemoveByNameAndVer[namever]) toRemoveByNameAndVer[namever] = []
+    toRemoveByNameAndVer[namever].push(flatname)
+  })
+  Object.keys(flatNewTree).forEach(function (path) {
+    var pkg = flatNewTree[path]
+    pkg.oldPkg = flatOldTree[path]
+    pkg.isInLink = (pkg.oldPkg && isLink(pkg.oldPkg.parent)) ||
+                   (pkg.parent && isLink(pkg.parent)) ||
+                   requiredByAllLinked(pkg)
+    if (pkg.fromBundle) return
+    if (pkg.oldPkg) {
+      if (!pkg.directlyRequested && pkgAreEquiv(pkg.oldPkg.package, pkg.package)) return
+      if (!pkg.isInLink && (isLink(pkg.oldPkg) || isLink(pkg))) {
+        differences.push(['update-linked', pkg])
+      } else {
+        differences.push(['update', pkg])
+      }
+    } else {
+      var vername = getNameAndVersion(pkg.package)
+      if (toRemoveByNameAndVer[vername] && toRemoveByNameAndVer[vername].length) {
+        var flatname = toRemoveByNameAndVer[vername].shift()
+        pkg.fromPath = toRemove[flatname].path
+        differences.push(['move', pkg])
+        delete toRemove[flatname]
+      } else {
+        differences.push(['add', pkg])
+      }
+    }
+  })
+  Object
+    .keys(toRemove)
+    .map(function (path) { return toRemove[path] })
+    .forEach(function (pkg) {
+      differences.push(['remove', pkg])
+    })
+  return differences
+}
---
+++ iCPS_Network_Manager/sfc-ui/module/node/npm/lib/install/logical-tree.js
@@ -0,0 +1,94 @@
+'use strict'
+var clone = require('lodash.clonedeep')
+var union = require('lodash.union')
+var without = require('lodash.without')
+var validate = require('aproba')
+var flattenTree = require('./flatten-tree.js')
+var isExtraneous = require('./is-extraneous.js')
+var validateAllPeerDeps = require('./deps.js').validateAllPeerDeps
+
+var logicalTree = module.exports = function (tree) {
+  validate('O', arguments)
+
+  var newTree = clone(tree)
+
+  validateAllPeerDeps(newTree, function (tree, pkgname, version) {
+    if (!tree.missingPeers) tree.missingPeers = {}
+    tree.missingPeers[pkgname] = version
+  })
+
+  var flat = flattenTree(newTree)
+
+  function getNode (flatname) {
+    return flatname.substr(0, 5) === '#DEV:' ?
+           flat[flatname.substr(5)] :
+           flat[flatname]
+  }
+
+  Object.keys(flat).sort().forEach(function (flatname) {
+    var node = flat[flatname]
+    var requiredBy = node.package._requiredBy || []
+    var requiredByNames = requiredBy.filter(function (parentFlatname) {
+      var parentNode = getNode(parentFlatname)
+      if (!parentNode) return false
+      return parentNode.package.dependencies[node.package.name] ||
+             (parentNode.package.devDependencies && parentNode.package.devDependencies[node.package.name])
+    })
+    requiredBy = requiredByNames.map(getNode)
+
+    node.requiredBy = requiredBy
+
+    if (!requiredBy.length) return
+
+    if (node.parent) node.parent.children = without(node.parent.children, node)
+
+    requiredBy.forEach(function (parentNode) {
+      parentNode.children = union(parentNode.children, [node])
+    })
+  })
+  return newTree
+}
+
+module.exports.asReadInstalled = function (tree) {
+  return translateTree(logicalTree(tree))
+}
+
+function translateTree (tree) {
+  var pkg = tree.package
+  if (pkg._dependencies) return pkg
+  pkg._dependencies = pkg.dependencies
+  pkg.dependencies = {}
+  tree.children.forEach(function (child) {
+    pkg.dependencies[child.package.name] = translateTree(child)
+  })
+  Object.keys(tree.missingDeps).forEach(function (name) {
+    if (pkg.dependencies[name]) {
+      pkg.dependencies[name].invalid = true
+      pkg.dependencies[name].realName = name
+      pkg.dependencies[name].extraneous = false
+    } else {
+      pkg.dependencies[name] = {
+        requiredBy: tree.missingDeps[name],
+        missing: true,
+        optional: !!pkg.optionalDependencies[name]
+      }
+    }
+  })
+  if (tree.missingPeers) {
+    Object.keys(tree.missingPeers).forEach(function (pkgname) {
+      var version = tree.missingPeers[pkgname]
+      pkg.dependencies[pkgname] = {
+        _id: pkgname + '@' + version,
+        name: pkgname,
+        version: version,
+        peerMissing: true
+      }
+    })
+  }
+  pkg.path = tree.path
+
+  pkg.error = tree.error
+  pkg.extraneous = isExtraneous(tree)
+  if (tree.target && tree.parent && !tree.parent.target) pkg.link = tree.realpath
+  return pkg
+}
